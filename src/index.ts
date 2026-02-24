0f27b4a50643c8d24a6ffc9ff5a9c2602b570b1a
Igor Gomes <roginaldosemog@gmail.com>
2026-02-24 15:58:51 -0300

    Remove módulos ios e web

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { PlugPagPluginPlugin } from './definitions';
 
-const PlugPagPlugin = registerPlugin<PlugPagPluginPlugin>('PlugPagPlugin', {
-  web: () => import('./web').then((m) => new m.PlugPagPluginWeb()),
-});
+const PlugPagPlugin = registerPlugin<PlugPagPluginPlugin>('PlugPagPlugin', {});
 
 export * from './definitions';
 export { PlugPagPlugin };
